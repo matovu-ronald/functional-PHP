3a93e48880fee621910cdcaee5824768a8232058
Matovu Ronald <maronwhite6@gmail.com>
2022-10-02 17:21:41 +0300

    Filtering

---
+++ filtering.php
@@ -0,0 +1,11 @@
+<?php
+
+$numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+$isEven = function ($number) {
+    return $number % 2 == 0;
+};
+
+$evenNumbers = array_filter($numbers, $isEven);
+
+print_r($evenNumbers);
